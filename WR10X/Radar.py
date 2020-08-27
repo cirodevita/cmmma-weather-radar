ebf97b5a6cafb9203bf3385885c46746d7757184
AlessioMaddaluno <alessio.maddaluno@gmail.com>
2020-08-27 12:14:22 +0200

    PEP8 restyling

--- WR10X/Radar.py
+++ WR10X/Radar.py
@@ -2,38 +2,31 @@ import os
 import sys
 import struct
 import json
+import warnings
 from datetime import datetime
+from .utils.statistical_filter import StatisticalFilter
 import numpy as np
 
-from .utils.statistical_filter import StatisticalFilter
-
-import warnings
 warnings.filterwarnings('ignore')
-
 np.set_printoptions(threshold=sys.maxsize)
 
+
 class Radar:
-
-    def __init__ (self, radar_config_file_path, scan_data):
-
+    def __init__(self, radar_config_file_path, scan_data):
         with open(radar_config_file_path) as f:
             config_file = json.load(f)
-        
 
-      
-        self._id       = config_file['radar_id']
-        self._location = (float(config_file['radar_location'][0]),float(config_file['radar_location'][1]))
-        self._kmdeg    = float(config_file['kmdeg'])
-        self._dir_data = os.path.join(config_file['dir_data'],scan_data)
+        self._id = config_file['radar_id']
+        self._location = (float(config_file['radar_location'][0]), float(config_file['radar_location'][1]))
+        self._kmdeg = float(config_file['kmdeg'])
+        self._dir_data = os.path.join(config_file['dir_data'], scan_data)
         self._config_file = config_file
 
         self._data = self.read_ppi_z_files()
-        
+
         self.apply_statistical_filter()
-        
         if self._config_file['sea_clutter'] is not None:
             self.remove_sea_clutter()
-        
         if self._config_file['com_map_path'] is not None:
             self.beam_blocking()
 
@@ -43,245 +36,191 @@ class Radar:
 
     def __str__(self):
         '''
-            Serializza la classe Radar mostrando le informazioni più
-            importanti.
+            Serialize the class
         '''
         return (f'Scan id: {self._scan_id}\n'
                 f'Name: {self._scan_name}\n'
                 f'Data: {self._scan_datestamp}\n'
                 f'Directory data: {self._dir_data}\n'
-                f'Radar location (lat,lon): {self._location[0]} {self._location[1]}\n'
-                f'Radar range (km): {self._range}\n'
-                f'Radar resolution (m): {self._resolution}\n'
+                f'Location (lat,lon): {self._location[0]} {self._location[1]}\n'
+                f'Range (km): {self._range}\n'
+                f'Resolution (m): {self._resolution}\n'
                 f'Beam per azimut: {self._ndata}')
 
     def read_ppi_z_files(self):
-        """Legge i dati relativi alla scansione del radar.
-        
-        La lettura è effettuata solo delle scansione cluttered. È necessario che sia
-        presente il file .Scan nella directory.
-
-        Returns
-        -------
-        upck_bytes : dictionary
-                     Contiene i valori di reflettività espressi in dbmz per ogni grado
-                     di elevazione. Il grado è utilizzato come key.
-
-        """
-
+        # Read scan data binaries and parse it.
         files = os.listdir(os.path.join(self._dir_data))
-        # Cerco il file .Scan
+        # Search for .Scan file
         self._scan_name = ''.join([f for f in files if f.endswith(('.Scan'))])
-        if not self._scan_name :
+        if not self._scan_name:
             raise FileNotFoundError
-        
-        # Leggo lo scan id
-        with open(os.path.join(self._dir_data,self._scan_name),'r') as sf:
+        # Read scan_ID
+        with open(os.path.join(self._dir_data, self._scan_name), 'r') as sf:
             self._scan_id = sf.read().strip()
-        
         # Scan data
         self._scan_datestamp = datetime.strptime(self._scan_name[4:16], '%Y%m%d%H%M')
-
-        # Leggo tutti i file binari
+        # Read binaries files
         bins = [f for f in files if f.endswith('-C.z') and f.startswith('PPI')]
         raw_bytes = {}
         for f in bins:
             el = f[39:41]
-            with open(os.path.join(self._dir_data,f),'rb') as file:
+            with open(os.path.join(self._dir_data, f), 'rb') as file:
                 raw_bytes[el] = file.read()
-
-        # Raggio del radar (km)
+        # Radar range (km)
         self._range = int(f[23:27])/10
-        # Risoluzione del radar (m)
+        # Radar resolution (m)
         self._resolution = int(f[28:32])
-        # Numero di beam lungo una direzione
+        # Bin per beam
         self._ndata = int(self._range*1000 / self._resolution)
-
-        # Effettuo un unpack di bytes in gruppi da 16 bit (ushort) in little endian
+        # Unpack data in 16 bytes (ushort) for group using little endian
         upck_bytes = {}
         for el in raw_bytes:
             upck_bytes[el] = struct.unpack('<' + str(len(raw_bytes[el]) // 2) + "H", raw_bytes[el])
-            
-            # Reshape dei dati
+            # Data reshape
             header_size = upck_bytes[el][0]
-            upck_bytes[el] =  np.reshape(upck_bytes[el] ,(self._ndata+header_size, 360), 'F')
-            
-            # Converto i dati in float32 (Requisito del filtro statistico)
+            upck_bytes[el] = np.reshape(upck_bytes[el], (self._ndata+header_size, 360), 'F')
             upck_bytes[el] = upck_bytes[el].astype('float32')
-            
-            # Converto i dati di dbz (dalla documentazione del WR10X)
+            # Converts data in dBZ (From WR10X doc)
             data_format = upck_bytes[el][header_size-1]
-            q_Z2Level = (pow(2,data_format)-1)*(-(-32)/(95.5-(-32)))
-            m_Z2Level = (pow(2,data_format)-1)/(95.5-(-32))
+            q_Z2Level = (pow(2, data_format)-1)*(-(-32)/(95.5-(-32)))
+            m_Z2Level = (pow(2, data_format)-1)/(95.5-(-32))
             upck_bytes[el] = (upck_bytes[el]-q_Z2Level)/m_Z2Level
-        
-            # Elimino gli header in ogni riga
+            # Delete headers
             upck_bytes[el] = upck_bytes[el][header_size:]
-            
-            # TODO : Verificare
-            # Soglie di riflettività sulle matrici raw
             upck_bytes[el][(upck_bytes[el] > 55)] = 55
 
         return upck_bytes
 
-
     def apply_statistical_filter(self):
-        '''
-            Applica il filtro statistico ai dati.
-            Parameters:
-             radar_data - dati della scansione
-        '''
+        # Converts dictionaries in lists
         data_mappe = []
         for el in self._data:
             data_mappe.append(self._data[el])
-
-        Mappe = np.array(data_mappe)
+        data = np.array(data_mappe)
+        # Reads threshold values
         Etn_Th = self._config_file['statistical_filter']['Etn_Th']
         Txt_Th = self._config_file['statistical_filter']['Txt_Th']
-        Z_Th   = self._config_file['statistical_filter']['Z_Th']
+        Z_Th = self._config_file['statistical_filter']['Z_Th']
 
-        d_filt1 = StatisticalFilter(Mappe, Etn_Th, Txt_Th, Z_Th)
-        # Scompongo in dati filtrati
+        d_filt1 = StatisticalFilter(data, Etn_Th, Txt_Th, Z_Th)
+        # Decompose data
         index = 0
         for el in self._data:
-            self._data[el] = d_filt1[index,:,:]
-            index+=1
+            self._data[el] = d_filt1[index, :, :]
+            index += 1
 
     def remove_sea_clutter(self):
 
         rd = np.empty([self._ndata, 360])
-
+        # Reads the levels and thresholds for applying
+        # the algorithm
         v1 = self._config_file['sea_clutter']['levels'][0]
         v2 = self._config_file['sea_clutter']['levels'][1]
         T1 = self._config_file['sea_clutter']['T1']
         T2 = self._config_file['sea_clutter']['T2']
-
+        # Reads in interval in which apply the algorithm
+        # (This reduce the number of operations)
         theta1 = self._config_file['sea_clutter']['interval'][0]
         theta2 = self._config_file['sea_clutter']['interval'][1]
         rho1 = self._config_file['sea_clutter']['interval'][2]
         rho2 = self._config_file['sea_clutter']['interval'][3]
 
-
         for i in range(self._ndata):
             for j in range(360):
-                rd[i, j] = self._data[v1][i,j] - self._data[v2][i,j]
+                rd[i, j] = self._data[v1][i, j] - self._data[v2][i, j]
         rd[rd == 0] = np.nan
 
         for j in range(theta1, theta2):
-            for i in range(rho1,rho2):
+            for i in range(rho1, rho2):
                 if (rd[i, j] > T1) or (rd[i, j] > 0.0 and self._data[v2][i, j] < T2):
                     self._data[v1][i, j] = self._data[v2][i, j]
 
-
     def beam_blocking(self):
-
+        '''
+            Reduce the beam occlusion caused by the orography
+        '''
         cm_path = self._config_file['com_map_path']
         files = os.listdir(cm_path)
-
+        # Read compensation maps
         MC = {}
         for f in files:
             if(f.startswith('mc')):
                 el = f.split('-')[1]
-                MC[el] = np.loadtxt(os.path.join(cm_path,f))
-
-        
+                MC[el] = np.loadtxt(os.path.join(cm_path, f))
+        # Apply the reflectivity increment
         for el in MC:
             for i in range(self._ndata):
                 for j in range(360):
-                    if self._data[el][i,j] <= 0.0:
-                        self._data[el][i,j] = np.nan
-                    self._data[el][i,j] += MC[el][i,j]
-
-
+                    if self._data[el][i, j] <= 0.0:
+                        self._data[el][i, j] = np.nan
+                    self._data[el][i, j] += MC[el][i, j]
 
     def apply_attenuation(self):
         '''
-            Correzione dell'attenuazione di percorso (PIA Algorithm)
+           Path Integrated Attenuation
         '''
         a = 0.000372
         b = 0.72
 
-        A   = {}
+        A = {}
         PIA = {}
         Z_filt = {}
 
-        # Conversione riflettivita in Z
+        # Converts reflectovity Z
         for el in self._data:
-            #self._data[el][( self._data[el] > 60)] = 60
-            self._data[el] = 10 ** ( self._data[el] / 10)
-
-            A[el]  = np.zeros([self._ndata, 360])
-            PIA[el]  = np.zeros([self._ndata, 360]) 
-            Z_filt[el]  = np.zeros([self._ndata, 360])
-        
-        
+            self._data[el] = 10 ** (self._data[el] / 10)
+            A[el] = np.zeros([self._ndata, 360])
+            PIA[el] = np.zeros([self._ndata, 360])
+            Z_filt[el] = np.zeros([self._ndata, 360])
+        # Calculate the attenuation coefficents
         for i in range(self._ndata):
             for j in range(360):
-                for el in  self._data:
-                    A[el][i,j] = abs(0.6 * a * ( self._data[el][i, j] ** b))
-                    A[el][np.isnan(A[el])] = 0.0
-
-        for i in range(self._ndata):
-            for j in range(0,360):
                 for el in self._data:
-                    PIA[el][i,j] = A[el][i, j] + PIA[el][i-1, j]
-        
-
+                    A[el][i, j] = abs(0.6 * a * (self._data[el][i, j] ** b))
+                    A[el][np.isnan(A[el])] = 0.0
+        # Calculate the PIA
+        for i in range(self._ndata):
+            for j in range(0, 360):
+                for el in self._data:
+                    PIA[el][i, j] = A[el][i, j] + PIA[el][i-1, j]
         for el in self._data:
-            PIA[el][(PIA[el]>10)] = 10
-        
-
+            PIA[el][(PIA[el] > 10)] = 10
+        # Calculate the correct reflectivity value
         for i in range(self._ndata):
             for j in range(360):
-                for el in  self._data:
-                    self._data[el][i,j] = 10*np.log10( self._data[el][i][j]) + PIA[el][i,j-1] + A[el][i,j]
+                for el in self._data:
+                    self._data[el][i, j] = 10*np.log10(self._data[el][i][j]) + PIA[el][i, j-1] + A[el][i, j]
+        for el in self._data:
+            self._data[el][(self._data[el] < 4)] = np.nan
 
-        for el in  self._data:
-            #radar_data[el] = 10*np.log10(radar_data[el])
-             self._data[el][( self._data[el] < 4)] = np.nan
-
-    
     def calculate_vmi(self):
-        '''
-            Calcola la VMI della scansione.
-        '''
+        # Converts data from dictionary to lists
         data = []
         for el in self._data:
             data.append(self._data[el])
-
+        # Calcualte the maximum along the vertical for
+        # each bin
         vmi = np.empty([self._ndata, 360])
         for i in range(self._ndata):
             for j in range(360):
-                vmi[i, j] = np.nanmax( [z[i][j] for z in data] )
-
-                if vmi[i, j] <= 0.0: 
+                vmi[i, j] = np.nanmax([z[i][j] for z in data])
+                if vmi[i, j] <= 0.0:
                     vmi[i, j] = np.nan
-
         return vmi
 
-
     def calculate_rain_rate(self):
-        '''
-            Calcola il rain_rate
-        '''
+        # Empirical coefficients for Z/R relationship
         a = 128.3
         b = 1.67
-
         vmi = self.calculate_vmi()
+        return pow(pow(10, vmi/10)/a, 1/b)
 
-        rain_rate = np.empty([self._ndata, 360])
-
-        rain_rate = pow(pow(10,vmi/10)/a,1/b)
-
-        return rain_rate
-
-    
     def calculate_poh(self):
 
-        el = ['01','02','03','04','05','07','10','12','15']
+        el = ['01', '02', '03', '04', '05', '07', '10', '12', '15']
         H = []
         for f in el:
-            H.append(np.loadtxt(os.path.join(self._config_file['H_dir'],f)))
+            H.append(np.loadtxt(os.path.join(self._config_file['H_dir'], f)))
 
         a = 3.44 * (10**-6)
         b = 4/7.0
@@ -295,8 +234,8 @@ class Radar:
 
         w = a * (Zf ** b)
 
-        VIL = w[0,:,:]*H[0]
-        for k in range(1,9):
+        VIL = w[0, :, :]*H[0]
+        for k in range(1, 9):
             VIL += w[k-1]*(H[k]-H[k-1]) + ((H[k]-H[k-1])*(w[k]-w[k-1]))/2
 
         VIL[VIL == 0] = np.nan
@@ -305,9 +244,9 @@ class Radar:
         for i in range(240):
             for j in range(360):
                 for k in range(6)[::-1]:
-                    if not np.isnan(w[k,i,j]):
+                    if not np.isnan(w[k, i, j]):
                         psv = H[k]
-                        echotop[i,j] = psv[i,j]
+                        echotop[i, j] = psv[i, j]
                         break
 
         VILD = np.array([240, 360])
@@ -320,22 +259,22 @@ class Radar:
 
         POH = ((d1 * (VILD ** 3) + d2 * (VILD ** 2) + d3 * (VILD) + d4)) * 100
         POH[POH < 0] = 0
+        POH[POH == 0] = np.nan
         POH[POH > 100] = 100
 
         return POH
 
-
     def create_grid(self):
         '''
-            Calcola il grigliato georiferito
+            Calculate the georeferenced grid
         '''
         ndata = self._ndata
 
         rkm = np.zeros(ndata)
         for j in range(ndata):
-            rkm[j] = self._range *j/ndata
-        
-        # angoli da 0 a 359 in radianti
+            rkm[j] = self._range * j/ndata
+
+        # Angles in radians
         z = np.zeros(360)
         for i in range(360):
             z[i] = np.pi*i/180
@@ -346,13 +285,9 @@ class Radar:
         for j in range(ndata):
             for i in range(360):
                 self.lat[i, j] = self._location[0] + np.cos(z[i]) * rkm[j] / self._kmdeg
-                self.lon[i, j] = self._location[1] + np.sin(z[i]) * (rkm[j] / self._kmdeg ) / np.cos(np.pi * self.lat[i, j] / 180.0)
-        
+                self.lon[i, j] = self._location[1] + np.sin(z[i]) * (rkm[j] / self._kmdeg) / np.cos(np.pi * self.lat[i, j] / 180.0)
+
         self.latmin = np.nanmin(self.lat)
         self.latmax = np.nanmax(self.lat)
         self.lonmin = np.nanmin(self.lon) - 0.02
         self.lonmax = np.nanmax(self.lon) + 0.02
-    
-
-   
-        
