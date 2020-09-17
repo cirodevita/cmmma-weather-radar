5e42ebe9cbd8e8aed5665e9ae8d59d53065acb94
AlessioMaddaluno <alessio.maddaluno@gmail.com>
2020-09-17 18:23:25 +0200

    fixed directory bug

--- WR10X/utils/bulk_unzip.py
+++ WR10X/utils/bulk_unzip.py
@@ -11,8 +11,9 @@ def unzipfile(filename,destfolder):
     try:
         Archive(filename).extractall(destfolder)
         print(f'{filename} extracted to {destfolder}')
-    except:
+    except ErrorName as e:
         print("Invalid cab file")
+        print(e)
     
 
 def bulk_unzip(input_path,output_path):
@@ -20,14 +21,19 @@ def bulk_unzip(input_path,output_path):
     files = os.listdir(input_path)
     for archive in files:
         if(archive.endswith('cab')):
-            x = os.path.join(output_path,archive[10:12],archive.split('.')[0])
+
+            day_dir = os.path.join(output_path,archive[10:12])
+            if not os.path.exists(day_dir):
+                os.makedirs(day_dir)
+
+            x = os.path.join(day_dir,archive.split('.')[0])
             unzipfile(os.path.join(input_path,archive),x)
     
 
 if __name__ == '__main__':
 
-    input_path = '/mnt/c/Users/Maimba/Desktop/DATA_RAW_11_2019'
-    output_path = '/mnt/c/Users/Maimba/Desktop/DATA_RAW'
+    input_path = '/mnt/c/Users/Maimba/Desktop/RAW_GIUGNO_AV'
+    output_path = '/mnt/c/Users/Maimba/Desktop/cmmma-weather-radar/data/AV/data'
    
     bulk_unzip(input_path,output_path)
 
